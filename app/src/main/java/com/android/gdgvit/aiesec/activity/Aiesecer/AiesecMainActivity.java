bcc17afffe76b167cb2301bfb682e702ded4b109
AKANSHI SRIVASTAVA <akanshi32@gmail.com>
2017-08-02 22:19:28 +0530

    ICX

--- app/src/main/java/com/android/gdgvit/aiesec/activity/Aiesecer/AiesecMainActivity.java
+++ app/src/main/java/com/android/gdgvit/aiesec/activity/Aiesecer/AiesecMainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.gdgvit.aiesec.R;
+
 public class AiesecMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
--- app/src/main/java/com/android/gdgvit/aiesec/activity/Main/ExpansionsFragment.java
+++ app/src/main/java/com/android/gdgvit/aiesec/activity/Main/ExpansionsFragment.java
@@ -1,12 +1,15 @@
 package com.android.gdgvit.aiesec.activity.Main;
 
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.android.gdgvit.aiesec.R;
 
@@ -19,91 +22,31 @@ import com.android.gdgvit.aiesec.R;
  * create an instance of this fragment.
  */
 public class ExpansionsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    ImageView imageView;
 
-    public ExpansionsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExpansionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ExpansionsFragment newInstance(String param1, String param2) {
-        ExpansionsFragment fragment = new ExpansionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root;
+        root = inflater.inflate(R.layout.fragment_expansions,container,false);
+
+        imageView = (ImageView) root.findViewById(R.id.oget);
+
+        AnimationDrawable animation = new AnimationDrawable();
+        animation.addFrame(getResources().getDrawable(R.drawable.exp1), 5000);
+        animation.addFrame(getResources().getDrawable(R.drawable.exp2), 5000);
+        animation.setOneShot(false);
+
+
+        imageView.setBackgroundDrawable(animation);
+
+        // start the animation!
+        animation.start();
+
+        return root;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expansions, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
